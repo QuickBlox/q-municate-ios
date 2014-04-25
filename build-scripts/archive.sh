bafa574f36846a6cc4db9ab08e61ab73632fe595
Andrey Moskvin <andrey.moskvin@quickblox.com>
2014-04-25 18:06:04 +0300

    Removed xcconfig corrected archive.sh
    Former-commit-id: bd4ceb9e0d7ea5400f8da461d3d6c7fc753d7609

--- build-scripts/archive.sh
+++ build-scripts/archive.sh
@@ -7,10 +7,10 @@
 #  Copyright (c) 2013 QuickBlox. All rights reserved.
 
 PROVISIONING_PROFILE_PATH="${WORK_DIR}/build/${PROVISIONING_NAME}.mobileprovision"
-SIGNING_IDENTITY="iPhone Distribution: Videu, LLC (82994PAQEW)"
-PRODUCT_NAME="Videu"
-APP="${WORK_DIR}/build/Videu.app"
-IPA="${WORK_DIR}/build/Videu.ipa"
+SIGNING_IDENTITY="iPhone Distribution: Injoit LTD (8885H5G2YX)"
+PRODUCT_NAME="Q-municate"
+APP="${WORK_DIR}/build/Q-municate.app"
+IPA="${WORK_DIR}/build/Q-municate.ipa"
 
 echo "Creating .ipa for ${PRODUCT_NAME}"
 /usr/bin/xcrun -sdk iphoneos PackageApplication -v "${APP}" -o "${IPA}" --sign "${SIGNING_IDENTITY}" --embed "${PROVISIONING_PROFILE_PATH}"
