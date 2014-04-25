e90fa326f4d5b9d8b113e08abdcd9b11a15ad4b6
Andrey Moskvin <andrey.moskvin@quickblox.com>
2014-04-25 17:48:15 +0300

    Corrected provisioning profile + corrected build sh
    Former-commit-id: 1a926a94e62c04d0366cd4145e122c245febcc22

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,1 +1,1 @@
-xcodebuild -xcconfig build-scripts/AdHoc.xcconfig -workspace Q-municate.xcworkspace/ -scheme Q-municate -configuration Release
+xcodebuild -xcconfig build-scripts/AdHoc.xcconfig -workspace Q-municate.xcworkspace/ -scheme Q-municate -configuration Release CONFIGURATION_BUILD_DIR=${bamboo.build.working.directory}/build
