a70d6dd0c25b96c6055039da6b64a86a9ef04489
Andrey Moskvin <andrey.moskvin@quickblox.com>
2014-04-25 17:50:17 +0300

    Corrected build.sj
    Former-commit-id: f5bdbbacfa38ceae1c5128ae2a17253ba1630d8f

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,1 +1,1 @@
-xcodebuild -xcconfig build-scripts/AdHoc.xcconfig -workspace Q-municate.xcworkspace/ -scheme Q-municate -configuration Release CONFIGURATION_BUILD_DIR=${bamboo.build.working.directory}/build
+xcodebuild -xcconfig build-scripts/AdHoc.xcconfig -workspace Q-municate.xcworkspace/ -scheme Q-municate -configuration Release CONFIGURATION_BUILD_DIR=../build
