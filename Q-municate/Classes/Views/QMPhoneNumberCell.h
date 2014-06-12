301789e25cb36c59fcdf3cb4e029f72fc4be4b99
Igor <igor.alefirenko@quickblox.com>
2014-06-12 15:14:37 +0300

    QB Framework deleted
    Former-commit-id: 56f26a8019dbded0dc17eafb72fbac8d468906e8

---
+++ Q-municate/Classes/Views/QMPhoneNumberCell.h
@@ -0,0 +1,15 @@
+//
+//  QMPhoneNumberCell.h
+//  Q-municate
+//
+//  Created by Igor Alefirenko on 11/06/2014.
+//  Copyright (c) 2014 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QMPhoneNumberCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *phoneNumbLabel;
+
+@end
---
+++ Q-municate/Classes/Views/QMPhoneNumberCell.m
@@ -0,0 +1,19 @@
+//
+//  QMPhoneNumberCell.m
+//  Q-municate
+//
+//  Created by Igor Alefirenko on 11/06/2014.
+//  Copyright (c) 2014 Quickblox. All rights reserved.
+//
+
+#import "QMPhoneNumberCell.h"
+
+@implementation QMPhoneNumberCell
+
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+@end
