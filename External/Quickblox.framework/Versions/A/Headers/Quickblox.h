22599fa88e584c9a5c1445dcf26df98b7f160504
Igor <igor.alefirenko@quickblox.com>
2014-09-08 14:51:26 +0300

    SDK updated, roaster is worked now
    Former-commit-id: f58a3b429bebfba9773811b16b76a15bc9b81e35

--- External/Quickblox.framework/Versions/A/Headers/Quickblox.h
+++ External/Quickblox.framework/Versions/A/Headers/Quickblox.h
@@ -4,14 +4,22 @@
 //
 //  Copyright (c) 2012 QuickBlox. All rights reserved.
 //
-// frameworkVersion 1.9.1
+// frameworkVersion 2.0.2
 
-#import "Core/BaseServiceFramework.h"
-#import "AuthService/AuthServiceFramework.h"
-#import "UsersService/UsersServiceFramework.h"
-#import "LocationService/LocationServiceFramework.h"
-#import "MessagesService/MessagesServiceFramework.h"
-#import "ContentService/Classes/ContentServiceFramework.h"
-#import "RatingsService/RatingsServiceFramework.h"
-#import "ChatService/ChatServiceFramework.h"
-#import "CustomObjects/CustomObjectsFramework.h"
+#import <Quickblox/BaseServiceFramework.h>
+#import <Quickblox/AuthServiceFramework.h>
+#import <Quickblox/UsersServiceFramework.h>
+#import <Quickblox/LocationServiceFramework.h>
+#import <Quickblox/MessagesServiceFramework.h>
+#import <Quickblox/ContentServiceFramework.h>
+#import <Quickblox/RatingsServiceFramework.h>
+#import <Quickblox/ChatServiceFramework.h>
+#import <Quickblox/CustomObjectsFramework.h>
+
+#import <Quickblox/QBCore.h>
+#import <Quickblox/QBAuthHeader.h>
+#import <Quickblox/QBLocationHeader.h>
+#import <Quickblox/QBUsersHeader.h>
+#import <Quickblox/QBMessagesHeader.h>
+#import <Quickblox/QBCustomObjectsHeader.h>
+#import <Quickblox/QBContentHeader.h>
