3f4af97eacc35ecdce83b5df0bb65f6269114885
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-10-06 12:36:58 +0300

    SearchDesplayController will now reload search result if user was added or deleted while it was active. This also fixed: QMUN-1295 issue

--- Q-municate/Classes/ViewControllers/TabBarController/QMFriendListController/QMFriendsListDataSource/QMFriendsListDataSource.m
+++ Q-municate/Classes/ViewControllers/TabBarController/QMFriendListController/QMFriendsListDataSource/QMFriendsListDataSource.m
@@ -86,6 +86,9 @@ QMContactListServiceDelegate
     if (self.viewIsShowed) {
         [self.tableView reloadData];
     }
+    if (self.searchDisplayController.isActive) {
+        [self globalSearch:self.searchDisplayController.searchBar.text];
+    }
 } 
 
 - (void)globalSearch:(NSString *)searchText {
@@ -156,11 +159,6 @@ QMContactListServiceDelegate
     else {
         [self reloadDataSource];
     }
-}
-
-- (void)reloadSearchData {
-    [self reloadDataSource];
-    [self globalSearch:self.searchDisplayController.searchBar.text];
 }
 
 #pragma mark - UITableViewDataSource
@@ -249,11 +247,11 @@ QMContactListServiceDelegate
     BOOL isContactRequest = [[QMApi instance] isContactRequestUserWithID:user.ID];
     if (isContactRequest) {
         [[QMApi instance] confirmAddContactRequest:user completion:^(BOOL success) {
-            [self reloadSearchData];
+            [self reloadDataSource];
         }];
     } else {
         [[QMApi instance] addUserToContactList:user completion:^(BOOL success, QBChatMessage *notification) {
-            [self reloadSearchData];
+            [self reloadDataSource];
         }];
     }
 }
