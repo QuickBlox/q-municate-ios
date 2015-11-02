2e31b1051601891a74116603d3fb0b921212a3f3
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-11-02 12:58:55 +0200

    Pods cleanup

--- Pods/QMServices/QMChatService/QMChatService/QMChatConstants.h
+++
@@ -1,9 +0,0 @@
-//
-//  QMChatConstants.h
-//  QMServices
-//
-//  Created by Injoit on 7/6/15.
-//  Copyright (c) 2015 Quickblox Team. All rights reserved.
-//
-
-#define kQMChatMessagesPerPage 100
