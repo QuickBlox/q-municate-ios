64c3b7674df4f0ce4cb104e1723e48ecd88520df
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-05 17:25:34 +0200

    fetchAllDialogs code improvements

--- Q-municate/Classes/Services/QMServicesFacade/QMApi+Chat.m
+++ Q-municate/Classes/Services/QMServicesFacade/QMApi+Chat.m
@@ -58,28 +58,22 @@
 - (void)fetchAllDialogs:(void(^)(void))completion {
 
     @weakify(self);
+    void (^iterationBlock)(QBResponse *, NSArray *, NSSet *, BOOL *) = ^(QBResponse *response, NSArray *dialogObjects, NSSet *dialogsUsersIDs, BOOL *stop) {
+        @strongify(self);
+        [self.usersService getUsersWithIDs:[dialogsUsersIDs allObjects]];
+    };
+    BFContinuationBlock completionBlock = ^id _Nullable(BFTask * _Nonnull task) {
+        @strongify(self);
+        if (self.isAuthorized && task.isCompleted) self.settingsManager.lastActivityDate = [NSDate date];
+        if (completion) completion();
+        return nil;
+    };
+    
     if (self.settingsManager.lastActivityDate != nil) {
-        [[self.chatService fetchDialogsUpdatedFromDate:self.settingsManager.lastActivityDate andPageLimit:kQMDialogsPageLimit iterationBlock:^(QBResponse *response, NSArray *dialogObjects, NSSet *dialogsUsersIDs, BOOL *stop) {
-            @strongify(self);
-            [self.usersService getUsersWithIDs:[dialogsUsersIDs allObjects]];
-        }] continueWithBlock:^id _Nullable(BFTask * _Nonnull task) {
-            @strongify(self);
-            if (self.isAuthorized && task.isCompleted) self.settingsManager.lastActivityDate = [NSDate date];
-            if (completion) completion();
-            return nil;
-        }];
+        [[self.chatService fetchDialogsUpdatedFromDate:self.settingsManager.lastActivityDate andPageLimit:kQMDialogsPageLimit iterationBlock:iterationBlock] continueWithBlock:completionBlock];
     }
     else {
-        [[self.chatService allDialogsWithPageLimit:kQMDialogsPageLimit extendedRequest:nil iterationBlock:^(QBResponse *response, NSArray *dialogObjects, NSSet *dialogsUsersIDs, BOOL *stop) {
-            @strongify(self);
-            [self.usersService getUsersWithIDs:[dialogsUsersIDs allObjects]];
-        }] continueWithBlock:^id _Nullable(BFTask * _Nonnull task) {
-            //
-            @strongify(self);
-            if (self.isAuthorized && task.isCompleted) self.settingsManager.lastActivityDate = [NSDate date];
-            if (completion) completion();
-            return nil;
-        }];
+        [[self.chatService allDialogsWithPageLimit:kQMDialogsPageLimit extendedRequest:nil iterationBlock:iterationBlock] continueWithBlock:completionBlock];
     }
 }
 
