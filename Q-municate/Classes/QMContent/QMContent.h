9bbead1a01bdd53c41b94f3c50f9d56700ec8896
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-08 16:21:13 +0200

    Implemented QMContent

---
+++ Q-municate/Classes/QMContent/QMContent.h
@@ -0,0 +1,38 @@
+//
+//  QMContent.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 1/8/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^QMContentProgressBlock)(float progress);
+
+/**
+ *  This class provides interface for content files.
+ */
+@interface QMContent : NSObject
+
+#pragma mark - Upload operations
+
++ (BFTask QB_GENERIC(QBCBlob *) *)uploadPNGImage:(UIImage *)image
+                  progress:(QMContentProgressBlock)progress;
+
++ (BFTask QB_GENERIC(QBCBlob *) *)uploadJPEGImage:(UIImage *)image
+                                         progress:(QMContentProgressBlock)progress;
+
++ (BFTask QB_GENERIC(QBCBlob *) *)uploadData:(NSData *)data
+                                    fileName:(NSString *)fileName
+                                 contentType:(NSString *)contentType
+                                    isPublic:(BOOL)isPublic
+                                    progress:(QMContentProgressBlock)progress;
+
+#pragma mark - Download operations
+
++ (BFTask QB_GENERIC(NSData *) *)downloadFileWithUrl:(NSURL *)url;
+
++ (BFTask QB_GENERIC(UIImage *) *)downloadImageWithUrl:(NSURL *)url;
+
+@end
