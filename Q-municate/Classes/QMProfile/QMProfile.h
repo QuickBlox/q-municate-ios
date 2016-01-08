02412732aa0b21e8d55f1a94f123b18bf559c24d
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-08 16:22:14 +0200

    Implemented QMProfile

---
+++ Q-municate/Classes/QMProfile/QMProfile.h
@@ -0,0 +1,35 @@
+//
+//  QMProfile.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 1/8/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  This class provides profile.
+ */
+@interface QMProfile : NSObject <NSCoding>
+
+@property (strong, nonatomic, QB_NULLABLE_PROPERTY) QBUUser *userData;
+@property (assign, nonatomic) BOOL userAgreementAccepted;
+@property (assign, nonatomic) BOOL pushNotificationsEnabled;
+
+/**
+ *  Returns loaded current profile with user.
+ *
+ *  @return current profile
+ */
++ (QB_NONNULL instancetype)currentProfile;
+
+- (BOOL)synchronize;
+
+- (BOOL)synchronizeWithUserData:(QB_NONNULL QBUUser *)userData;
+
+- (void)loadProfile;
+
+- (BOOL)clearProfile;
+
+@end
