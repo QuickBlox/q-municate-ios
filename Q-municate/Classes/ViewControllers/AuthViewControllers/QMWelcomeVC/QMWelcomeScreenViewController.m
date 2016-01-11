fc6bfc9ef6cfadf0fa9b5d13a6c87d58fd0777fe
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-11 14:20:17 +0200

    Fixed BFTasks success check

--- Q-municate/Classes/ViewControllers/AuthViewControllers/QMWelcomeVC/QMWelcomeScreenViewController.m
+++ Q-municate/Classes/ViewControllers/AuthViewControllers/QMWelcomeVC/QMWelcomeScreenViewController.m
@@ -47,18 +47,17 @@
     @weakify(self);
     [[[[[[[QMFacebook connect] continueWithBlock:^id _Nullable(BFTask<NSString *> * _Nonnull task) {
         // Facebook connect
-        return task.isCompleted ? [[QMCore instance].authService loginWithFacebookSessionToken:task.result] : nil;
+        return task.isFaulted ? nil : [[QMCore instance].authService loginWithFacebookSessionToken:task.result];
     }] continueWithBlock:^id _Nullable(BFTask<QBUUser *> * _Nonnull task) {
         //
-        if (task.isCompleted) {
+        if (task.isFaulted) {
+            [REAlertView showAlertWithMessage:NSLocalizedString(@"QM_STR_FACEBOOK_LOGIN_FALED_ALERT_TEXT", nil) actionSuccess:NO];
+        } else {
             @strongify(self);
             [self performSegueWithIdentifier:kQMSceneSegueMain sender:nil];
             
             [[QMCore instance].currentProfile synchronizeWithUserData:task.result];
             if (task.result.avatarUrl.length == 0) return [QMFacebook loadMe];
-        } else {
-            //
-            [REAlertView showAlertWithMessage:NSLocalizedString(@"QM_STR_FACEBOOK_LOGIN_FALED_ALERT_TEXT", nil) actionSuccess:NO];
         }
         
         return nil;
