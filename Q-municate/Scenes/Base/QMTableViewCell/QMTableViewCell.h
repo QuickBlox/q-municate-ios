258d92a7e35147780857392bf9f643f831547491
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-13 16:17:46 +0200

    Added QMTableViewCell

---
+++ Q-municate/Scenes/Base/QMTableViewCell/QMTableViewCell.h
@@ -0,0 +1,21 @@
+//
+//  QMTableViewCell.h
+//  Q-municate
+//
+//  Created by Andrey Ivanov on 23.03.15.
+//  Copyright (c) 2015 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QMTableViewCell : UITableViewCell
+
++ (void)registerForReuseInTableView:(UITableView *)tableView;
++ (NSString *)cellIdentifier;
++ (CGFloat)height;
+
+- (void)setAvatarWithUrl:(NSString *)avatarUrl;
+- (void)setTitle:(NSString *)title;
+- (void)setBody:(NSString *)body;
+
+@end
