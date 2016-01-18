82ee538754a3f61445e9bb5348ec260693c7aa43
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-01-18 15:08:36 +0200

    Updated QMChatUtils + added occupantsWithoutCurrentUser utility method

--- Q-municate/Classes/Tools/QMChatUtils/QMChatUtils.m
+++ Q-municate/Classes/Tools/QMChatUtils/QMChatUtils.m
@@ -7,7 +7,7 @@
 //
 
 #import "QMChatUtils.h"
-#import "QMApi.h"
+#import "QMCore.h"
 
 @implementation QMChatUtils
 
@@ -15,31 +15,31 @@
 + (NSString *)messageTextForNotification:(QBChatMessage *)notification
 {
     NSString *messageText = nil;
-    QBUUser *sender = [[QMApi instance] userWithID:notification.senderID];
-    QBUUser *recipient = [[QMApi instance] userWithID:notification.recipientID];
+    QBUUser *sender = [[QMCore instance].usersService.usersMemoryStorage userWithID:notification.senderID];
+    QBUUser *recipient = [[QMCore instance].usersService.usersMemoryStorage userWithID:notification.recipientID];
     
     switch (notification.messageType) {
         case QMMessageTypeContactRequest:
         {
-            messageText = (notification.senderID == QMApi.instance.currentUser.ID) ?  NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_SEND_FOR_ME",nil) : [NSString stringWithFormat:NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_SEND_FOR_OPPONENT", @"{FullName}"), sender.fullName];
+            messageText = (notification.senderID == [QMCore instance].currentUser.ID) ?  NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_SEND_FOR_ME",nil) : [NSString stringWithFormat:NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_SEND_FOR_OPPONENT", @"{FullName}"), sender.fullName];
         }
             break;
             
         case QMMessageTypeAcceptContactRequest:
         {
-            messageText = (notification.senderID == QMApi.instance.currentUser.ID) ? NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_CONFIRM_FOR_ME", nil) : NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_CONFIRM_FOR_OPPONENT", nil);
+            messageText = (notification.senderID == [QMCore instance].currentUser.ID) ? NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_CONFIRM_FOR_ME", nil) : NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_CONFIRM_FOR_OPPONENT", nil);
         }
             break;
             
         case QMMessageTypeRejectContactRequest:
         {
-            messageText = (notification.senderID == QMApi.instance.currentUser.ID) ? NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_REJECT_FOR_ME",nil) : NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_REJECT_FOR_OPPONENT", nil);
+            messageText = (notification.senderID == [QMCore instance].currentUser.ID) ? NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_REJECT_FOR_ME",nil) : NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_REJECT_FOR_OPPONENT", nil);
         }
             break;
             
         case QMMessageTypeDeleteContactRequest:
         {
-            messageText = (notification.senderID == QMApi.instance.currentUser.ID) ?
+            messageText = (notification.senderID == [QMCore instance].currentUser.ID) ?
             [NSString stringWithFormat:NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_DELETE_FOR_ME", @"{FullName}"), recipient.fullName] :
             [NSString stringWithFormat:NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_DELETE_FOR_OPPONENT", @"{FullName}"), sender.fullName];
         }
@@ -60,12 +60,12 @@
                 {
                     if ([notification.addedOccupantsIDs count] > 0) {
                         
-                        NSArray *users = [[QMApi instance] usersWithIDs:notification.addedOccupantsIDs];
+                        NSArray *users = [[QMCore instance].usersService.usersMemoryStorage usersWithIDs:notification.addedOccupantsIDs];
                         NSString *fullNameString = [self fullNamesString:users];
                         messageText = [NSString stringWithFormat:NSLocalizedString(@"QM_STR_ADD_USERS_TO_EXIST_GROUP_CONVERSATION_TEXT", nil), sender.fullName, fullNameString];
                     } else if ([notification.deletedOccupantsIDs count] > 0) {
                         
-                        QBUUser *leavedUser = [[QMApi instance] userWithID:[[notification.deletedOccupantsIDs firstObject] integerValue]];
+                        QBUUser *leavedUser = [[QMCore instance].usersService.usersMemoryStorage userWithID:[[notification.deletedOccupantsIDs firstObject] integerValue]];
                         messageText = [NSString stringWithFormat:NSLocalizedString(@"QM_STR_LEAVE_GROUP_CONVERSATION_TEXT", nil), leavedUser.fullName];
                     }
                 }
@@ -85,7 +85,7 @@
 + (NSString *)messageTextForPushWithNotification:(QBChatMessage *)notification
 {
     NSString *message = nil;
-    QBUUser *sender = [[QMApi instance] userWithID:notification.senderID];
+    QBUUser *sender = [[QMCore instance].usersService.usersMemoryStorage userWithID:notification.senderID];
     if (notification.messageType == QMMessageTypeContactRequest) {
         message = [NSString stringWithFormat:NSLocalizedString(@"QM_STR_FRIEND_REQUEST_DID_SEND_FOR_OPPONENT", @"{FullName}"), sender.fullName];
     } else if (notification.messageType == QMMessageTypeAcceptContactRequest) {
@@ -127,7 +127,19 @@
 + (NSString *)messageForText:(NSString *)text participants:(NSArray *)participants
 {
     NSString *addedUsersNames = [self fullNamesString:participants];
-    return [NSString stringWithFormat:text, [QMApi instance].currentUser.fullName, addedUsersNames];
+    return [NSString stringWithFormat:text, [QMCore instance].currentUser.fullName, addedUsersNames];
+}
+
++ (NSArray *)occupantsWithoutCurrentUser:(NSArray *)occupantIDs {
+    
+    NSMutableArray *occupantsWithoutCurrentUser = [NSMutableArray array];
+    for (NSNumber *identifier in occupantIDs) {
+        if (![identifier isEqualToNumber:@([QMCore instance].currentUser.ID)]) {
+            [occupantsWithoutCurrentUser addObject:identifier];
+        }
+    }
+    
+    return [occupantsWithoutCurrentUser copy];
 }
 
 @end
