55feef72037198ee8017bdefbd62bbae63d293d4
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-03-01 17:50:06 +0200

    WIP QMGlobalSearchDataSource

---
+++ Q-municate/Scenes/Main/QMDialogsViewController/QMGlobalSearchDataSource/QMGlobalSearchDataSource.h
@@ -0,0 +1,13 @@
+//
+//  QMGlobalSearchDataSource.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 3/1/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMTableViewDataSource.h"
+
+@interface QMGlobalSearchDataSource : QMTableViewDataSource
+
+@end
---
+++ Q-municate/Scenes/Main/QMDialogsViewController/QMGlobalSearchDataSource/QMGlobalSearchDataSource.m
@@ -0,0 +1,13 @@
+//
+//  QMGlobalSearchDataSource.m
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 3/1/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMGlobalSearchDataSource.h"
+
+@implementation QMGlobalSearchDataSource
+
+@end
