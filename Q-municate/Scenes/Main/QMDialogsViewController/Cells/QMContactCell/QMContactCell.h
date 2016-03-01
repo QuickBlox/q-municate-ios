24d01893aa259513f6c762a64e735ae5362215e6
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-03-01 17:46:56 +0200

    Implemented QMContactCell

---
+++ Q-municate/Scenes/Main/QMDialogsViewController/Cells/QMContactCell/QMContactCell.h
@@ -0,0 +1,15 @@
+//
+//  QMContactCell.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 3/1/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMTableViewCell.h"
+
+@interface QMContactCell : QMTableViewCell
+
+- (void)setIsUserFriend:(BOOL)isUserFriend;
+
+@end
---
+++ Q-municate/Scenes/Main/QMDialogsViewController/Cells/QMContactCell/QMContactCell.m
@@ -0,0 +1,33 @@
+//
+//  QMContactCell.m
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 3/1/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMContactCell.h"
+
+@interface QMContactCell ()
+
+@property (weak, nonatomic) IBOutlet UIButton *addFriendButton;
+
+@end
+
+@implementation QMContactCell
+
++ (NSString *)cellIdentifier {
+    
+    return @"QMChatCell";
+}
+
+#pragma mark - setters
+
+- (void)setIsUserFriend:(BOOL)isUserFriend {
+    
+    self.addFriendButton.hidden = isUserFriend;
+}
+
+#pragma mark - action
+
+@end
