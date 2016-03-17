fd33585ef174df8af3ba4441293568182cc6986f
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-03-17 16:36:11 +0200

    Updated pods

--- Pods/QMCVDevelopment/QMChatViewController/Utils/QMDateUtils/QMDateUtils.m
+++ Pods/QMCVDevelopment/QMChatViewController/Utils/QMDateUtils/QMDateUtils.m
@@ -1,63 +1,146 @@
 //
 //  QMDateUtils.m
-//  Pods
+//  QMChatViewController
 //
 //  Created by Vitaliy Gorbachov on 11/19/15.
-//
+//  Copyright (c) 2015 QuickBlox Team. All rights reserved.
 //
 
 #import "QMDateUtils.h"
 
 @implementation QMDateUtils
 
-+ (NSDateFormatter *)dateFormatter
-{
-    static NSDateFormatter* formatter = nil;
-    
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateFormat = @"HH:mm";
-    });
-    
-    return formatter;
-}
-
 + (NSString *)formattedStringFromDate:(NSDate *)date
 {
     NSString *formattedString = nil;
-    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:date];
-    NSDateComponents *currentComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:date];
+    NSDateComponents *currentComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
     
-    NSString *sectionDate = [[self dateFormatter] stringFromDate:date];
+    NSString *sectionDate = [self formatDateForTimeRange:date];
     
     if (components.day == currentComponents.day && components.month == currentComponents.month && components.year == currentComponents.year) {
-        formattedString = [NSString stringWithFormat:@"Today %@", sectionDate];
-    } else if (components.day == currentComponents.day-1 && components.month == currentComponents.month && components.year == currentComponents.year) {
-        formattedString = [NSString stringWithFormat:@"Yesterday %@", sectionDate];
-    } else if (components.year == currentComponents.year) {
-        formattedString = [NSString stringWithFormat:@"%@ %ld %@", [self monthFromNumber:components.month], (long)components.day, sectionDate];
-    } else {
-        formattedString = [NSString stringWithFormat:@"%@ %ld %ld %@", [self monthFromNumber:components.month], (long)components.day, (long)components.year, sectionDate];
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@", [self formatDateForDayRange:date], sectionDate];
     }
+    else if (components.day == currentComponents.day - 1 && components.month == currentComponents.month && components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@", [self formatDateForDayRange:date], sectionDate];
+    }
+    else if (components.weekOfMonth == currentComponents.weekOfMonth && components.month == currentComponents.month && components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@", [self formatDateForWeekRange:date], sectionDate];
+    }
+    else if (components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@", [self formatDateForMonthRange:date], sectionDate];
+    }
+    else {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@", [self formatDateForYearRange:date], sectionDate];
+    }
+    
     return formattedString;
 }
 
-+ (NSString *)monthFromNumber:(NSInteger)number
++ (NSString *)formattedLastSeenString:(NSDate *)date withTimePrefix:(NSString *)timePrefix
 {
-    NSDictionary *dict = @{@1: @"January",
-                           @2: @"February",
-                           @3: @"March",
-                           @4: @"April",
-                           @5: @"May",
-                           @6: @"June",
-                           @7: @"July",
-                           @8: @"August",
-                           @9: @"September",
-                           @10: @"October",
-                           @11: @"November",
-                           @12: @"December"};
-    return dict[@(number)];
+    NSString *formattedString = nil;
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:date];
+    NSDateComponents *currentComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
+    
+    if (components.day == currentComponents.day && components.month == currentComponents.month && components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@ %@", [self formatDateForDayRange:date], timePrefix, [self formatDateForTimeRange:date]];
+    }
+    else if (components.day == currentComponents.day - 1 && components.month == currentComponents.month && components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@ %@", [self formatDateForDayRange:date], timePrefix, [self formatDateForTimeRange:date]];
+    }
+    else if (components.weekOfMonth == currentComponents.weekOfMonth && components.month == currentComponents.month && components.year == currentComponents.year) {
+        
+        formattedString = [NSString stringWithFormat:@"%@ %@ %@", [self formatDateForWeekRange:date], timePrefix, [self formatDateForTimeRange:date]];
+    }
+    else {
+        
+        formattedString = [NSString stringWithFormat:@"%@", [self formatDateForString:date]];
+    }
+    
+    return formattedString;
+}
+
++ (NSString *)formatDateForTimeRange:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateStyle = NSDateFormatterNoStyle;
+        formatter.timeStyle = NSDateFormatterShortStyle;
+    });
+    
+    return [formatter stringFromDate:date];
+}
+
++ (NSString *)formatDateForDayRange:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateStyle = NSDateFormatterMediumStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+        formatter.doesRelativeDateFormatting = YES;
+    });
+    
+    return [formatter stringFromDate:date];
+}
+
++ (NSString *)formatDateForWeekRange:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"EEEE";
+    });
+    
+    return [formatter stringFromDate:date];
+}
+
++ (NSString *)formatDateForMonthRange:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"LLLL d";
+    });
+    
+    return [formatter stringFromDate:date];
+}
+
++ (NSString *)formatDateForYearRange:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"LLLL d y";
+    });
+    
+    return [formatter stringFromDate:date];
+}
+
++ (NSString *)formatDateForString:(NSDate *)date
+{
+    static NSDateFormatter* formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"d/MM/y";
+    });
+    
+    return [formatter stringFromDate:date];
 }
 
 @end
