bedc0040509f28db25034c86cca90503b0a3f4c4
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-03-28 11:44:32 +0300

    Updated Localizable strings, xcode project file, segue constants and identifiers, chat storyboard

--- Q-municate/QMConstants.h
+++ Q-municate/QMConstants.h
@@ -9,6 +9,9 @@
 #ifndef QMConstants_h
 #define QMConstants_h
 
+// storyboards
+static NSString *const kQMChatStoryboard = @"Chat";
+
 static NSString *const kQMQBUUserFullNameKeyPathKey = @"fullName";
 static NSString *const kQMDialogsUpdateNotificationMessage = @"Notification message";
 static NSString *const kQMContactRequestNotificationMessage = @"Contact request";
