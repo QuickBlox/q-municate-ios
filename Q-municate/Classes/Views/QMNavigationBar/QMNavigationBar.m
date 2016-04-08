832db3223ae01db73bdea69e54b7050e3846e9bd
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-04-08 10:53:41 +0300

    WIP

---
+++ Q-municate/Classes/Views/QMNavigationBar/QMNavigationBar.m
@@ -0,0 +1,21 @@
+//
+//  QMNavigationBar.m
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 4/1/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMNavigationBar.h"
+#import "QMNotificationPanel.h"
+#import <QMImageView.h>
+
+@interface QMNavigationBar ()
+
+
+@end
+
+@implementation QMNavigationBar
+
+
+@end
