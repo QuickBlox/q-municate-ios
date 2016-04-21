d04dcaebcfeac52b75fea17ff32f53a77ecef38d
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-04-21 16:13:32 +0300

    Implemented QMAddMemberCell, QMLeaveChatCell and QMSeparatorCell

---
+++ Q-municate/Scenes/Cells/QMAddMemberCell/QMAddMemberCell.h
@@ -0,0 +1,13 @@
+//
+//  QMAddMemberCell.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 4/5/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMTableViewCell.h"
+
+@interface QMAddMemberCell : QMTableViewCell
+
+@end
---
+++ Q-municate/Scenes/Cells/QMAddMemberCell/QMAddMemberCell.m
@@ -0,0 +1,29 @@
+//
+//  QMAddMemberCell.m
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 4/5/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMAddMemberCell.h"
+#import "QMShadowView.h"
+
+@implementation QMAddMemberCell
+
++ (CGFloat)height {
+    
+    return 50.0f;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    QMShadowView *shadowView = [[QMShadowView alloc] initWithFrame:CGRectMake(0,
+                                                                              0,
+                                                                              CGRectGetWidth(self.frame),
+                                                                              kQMShadowViewHeight)];
+    [self.contentView addSubview:shadowView];
+}
+
+@end
