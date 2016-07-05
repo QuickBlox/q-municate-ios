f5dff71813b1f2b3b8533af7db9f25d710b20917
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-07-05 16:03:42 +0300

    QMLocationPinView

---
+++ Q-municate/Scenes/Views/QMLocationPinView.h
@@ -0,0 +1,35 @@
+//
+//  QMLocationPinView.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 7/5/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  Pin origin center.
+ *
+ *  @note Use this to shift pin view on X point.
+ */
+extern const CGFloat QMLocationPinViewOriginPinCenter;
+
+@interface QMLocationPinView : UIView
+
+/**
+ *  Whether pin is rised or not.
+ *
+ *  @note Setting this property to a positive value will perform pin set with no animation.
+ */
+@property (assign, nonatomic) BOOL pinRaised;
+
+/**
+ *  Set pin rised.
+ *
+ *  @param pinRaised whether pin should be rised or not
+ *  @param animated  whether rise should be performed with animation
+ */
+- (void)setPinRaised:(BOOL)pinRaised animated:(BOOL)animated;
+
+@end
