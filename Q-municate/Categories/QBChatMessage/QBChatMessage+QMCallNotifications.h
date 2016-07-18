298dbaf1c6cb4fea6e7c3c537a0e10bc5e215397
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-07-18 11:30:46 +0300

    Call notification logic

---
+++ Q-municate/Categories/QBChatMessage/QBChatMessage+QMCallNotifications.h
@@ -0,0 +1,38 @@
+//
+//  QBChatMessage+QMCallNotifications.h
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 7/13/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, QMCallNotificationType) {
+    
+    QMCallNotificationTypeNone,
+    QMCallNotificationTypeAudio,
+    QMCallNotificationTypeVideo
+};
+
+typedef NS_ENUM(NSUInteger, QMCallNotificationState) {
+    
+    QMCallNotificationStateNone,
+    QMCallNotificationStateHangUp,
+    QMCallNotificationStateMissedNoAnswer,
+    QMCallNotificationStateReject,
+};
+
+@interface QBChatMessage (QMCallNotifications)
+
+@property (assign, nonatomic) QMCallNotificationType callNotificationType;
+@property (assign, nonatomic) QMCallNotificationState callNotificationState;
+
+@property (assign, nonatomic) NSUInteger callerUserID;
+@property (strong, nonatomic) NSIndexSet *calleeUserIDs;
+
+@property (assign, nonatomic) NSTimeInterval callDuration;
+
+- (BOOL)isCallNotificationMessage;
+
+@end
---
+++ Q-municate/Classes/QMCallNotificationItem/QMCallNotificationItem.m
@@ -0,0 +1,183 @@
+//
+//  QMCallNotificationItem.m
+//  Q-municate
+//
+//  Created by Vitaliy Gorbachov on 7/14/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QMCallNotificationItem.h"
+#import "QMCore.h"
+
+#import "QBChatMessage+QMCallNotifications.h"
+
+static UIImage *missedAudioIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-audio-missing"];
+    });
+    
+    return image;
+}
+
+static UIImage *missedVideoIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-video-missing"];
+    });
+    
+    return image;
+}
+
+static UIImage *outgoingAudioIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-audio-outgoing"];
+    });
+    
+    return image;
+}
+
+static UIImage *incomingAudioIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-audio-incoming"];
+    });
+    
+    return image;
+}
+
+static UIImage *outgoingVideoIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-video-outgoing"];
+    });
+    
+    return image;
+}
+
+static UIImage *incomingVideoIcon() {
+    
+    static UIImage *image = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        image = [UIImage imageNamed:@"qm-ic-video-incoming"];
+    });
+    
+    return image;
+}
+
+@implementation QMCallNotificationItem
+
+- (instancetype)initWithCallNotificationMessage:(QBChatMessage *)message {
+    
+    self = [super init];
+    if (self != nil) {
+        
+        switch (message.callNotificationState) {
+            
+            case QMCallNotificationStateHangUp: {
+                
+                if (message.callerUserID == [QMCore instance].currentProfile.userData.ID) {
+                    
+                    if (message.callNotificationType == QMCallNotificationTypeAudio) {
+                        
+                        _notificationText = NSLocalizedString(@"QM_STR_OUTGOING_CALL", nil);
+                        _iconImage = outgoingAudioIcon();
+                    }
+                    else {
+                        
+                        _notificationText = NSLocalizedString(@"QM_STR_OUTGOING_VIDEO_CALL", nil);
+                        _iconImage = outgoingVideoIcon();
+                    }
+                }
+                else {
+                    
+                    if (message.callNotificationType == QMCallNotificationTypeAudio) {
+                        
+                        _notificationText = NSLocalizedString(@"QM_STR_INCOMING_CALL", nil);
+                        _iconImage = incomingAudioIcon();
+                    }
+                    else {
+                        
+                        _notificationText = NSLocalizedString(@"QM_STR_INCOMING_VIDEO_CALL", nil);
+                        _iconImage = incomingVideoIcon();
+                    }
+                }
+                
+                double minutes = floor(message.callDuration/60);
+                double seconds = round(message.callDuration - minutes * 60);
+                NSString *timeStr = [NSString stringWithFormat:@"%tu:%tu", (NSUInteger)minutes, (NSUInteger)seconds];
+                
+                _notificationText = [NSString stringWithFormat:@"%@, %@", _notificationText, timeStr];
+                
+                break;
+            }
+                
+            case QMCallNotificationStateMissedNoAnswer: {
+                
+                if (message.callNotificationType == QMCallNotificationTypeAudio) {
+                    
+                    _iconImage = missedAudioIcon();
+                    
+                    _notificationText = message.callerUserID == [QMCore instance].currentProfile.userData.ID ? NSLocalizedString(@"QM_STR_NO_ANSWER", nil) : NSLocalizedString(@"QM_STR_MISSED_CALL", nil);
+                }
+                else {
+                    
+                    _iconImage = missedVideoIcon();
+                    
+                    _notificationText = message.callerUserID == [QMCore instance].currentProfile.userData.ID ? NSLocalizedString(@"QM_STR_NO_ANSWER", nil) : NSLocalizedString(@"QM_STR_MISSED_VIDEO_CALL", nil);
+                }
+                
+                break;
+            }
+                
+            case QMCallNotificationStateReject: {
+                
+                if (message.callNotificationType == QMCallNotificationTypeAudio) {
+                    
+                    _iconImage = missedAudioIcon();
+                    
+                    _notificationText = NSLocalizedString(@"QM_STR_REJECTED_CALL", nil);
+                }
+                else {
+                    
+                    _iconImage = missedVideoIcon();
+                    
+                    _notificationText = NSLocalizedString(@"QM_STR_REJECTED_VIDEO_CALL", nil);
+                }
+                
+                break;
+            }
+                
+            case QMCallNotificationStateNone:
+                break;
+        }
+    }
+    
+    return self;
+}
+
+@end
