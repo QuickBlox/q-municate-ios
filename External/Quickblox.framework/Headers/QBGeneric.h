57ba870d0efb76572b6aa667deb527ef37cdebe4
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2016-11-22 16:36:20 +0200

    updated SDK

--- External/Quickblox.framework/Headers/QBGeneric.h
+++ External/Quickblox.framework/Headers/QBGeneric.h
@@ -9,10 +9,10 @@
 #ifndef Quickblox_QBGeneric_h
 #define Quickblox_QBGeneric_h
 
-//#if __has_feature(objc_generics) || __has_extension(objc_generics)
-//#  define QB_GENERIC(...) <__VA_ARGS__>
-//#else
-//#  define QB_GENERIC(...)
-//#endif
+#if __has_feature(objc_generics) || __has_extension(objc_generics)
+#  define QB_GENERIC(...) <__VA_ARGS__>
+#else
+#  define QB_GENERIC(...)
+#endif
 
 #endif /* QBGeneric_h */
--- Q-municate/QMAppDelegate.m
+++ Q-municate/QMAppDelegate.m
@@ -195,76 +195,76 @@ static NSString * const kQMAccountKey = @"6Qyiz3pZfNsex1Enqnp7";
     [QMCore instance].pushNotificationManager.deviceToken = deviceToken;
 }
 
-- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo withResponseInfo:(NSDictionary *)responseInfo completionHandler:(void (^)())completionHandler {
-    
-    if ([identifier isEqualToString:kQMNotificationActionTextAction]) {
-        
-        NSString *text = responseInfo[UIUserNotificationActionResponseTypedTextKey];
-        
-        NSCharacterSet *whiteSpaceSet = [NSCharacterSet whitespaceCharacterSet];
-        if ([text stringByTrimmingCharactersInSet:whiteSpaceSet].length == 0) {
-            // do not send message that contains only of spaces
-            if (completionHandler) {
-                
-                completionHandler();
-            }
-            
-            return;
-        }
-        
-        NSString *dialogID = userInfo[kQMPushNotificationDialogIDKey];
-        
-        __block UIBackgroundTaskIdentifier task = [application beginBackgroundTaskWithExpirationHandler:^{
-            
-            [application endBackgroundTask:task];
-            task = UIBackgroundTaskInvalid;
-        }];
-        
-        // Do the work associated with the task.
-        ILog(@"Started background task timeremaining = %f", [application backgroundTimeRemaining]);
-        
-        [[[QMCore instance].chatService fetchDialogWithID:dialogID] continueWithBlock:^id _Nullable(BFTask<QBChatDialog *> * _Nonnull t) {
-            
-            QBChatDialog *chatDialog = t.result;
-            if (chatDialog != nil) {
-                
-                NSUInteger opponentUserID = [userInfo[kQMPushNotificationUserIDKey] unsignedIntegerValue];
-                
-                if (chatDialog.type == QBChatDialogTypePrivate
-                    && ![[QMCore instance].contactManager isFriendWithUserID:opponentUserID]) {
-                    
-                    if (completionHandler) {
-                        
-                        completionHandler();
-                    }
-                    
-                    return nil;
-                }
-                
-                return [[[QMCore instance].chatManager sendBackgroundMessageWithText:text toDialogWithID:dialogID] continueWithBlock:^id _Nullable(BFTask * _Nonnull messageTask) {
-                    
-                    if (!messageTask.isFaulted
-                        && application.applicationIconBadgeNumber > 0) {
-                        
-                        application.applicationIconBadgeNumber = 0;
-                    }
-                    
-                    [application endBackgroundTask:task];
-                    task = UIBackgroundTaskInvalid;
-                    
-                    return nil;
-                }];
-            }
-            
-            return nil;
-        }];
-    }
-    
-    if (completionHandler) {
-        
-        completionHandler();
-    }
-}
+//- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo withResponseInfo:(NSDictionary *)responseInfo completionHandler:(void (^)())completionHandler {
+
+//    if ([identifier isEqualToString:kQMNotificationActionTextAction]) {
+//        
+//        NSString *text = responseInfo[UIUserNotificationActionResponseTypedTextKey];
+//        
+//        NSCharacterSet *whiteSpaceSet = [NSCharacterSet whitespaceCharacterSet];
+//        if ([text stringByTrimmingCharactersInSet:whiteSpaceSet].length == 0) {
+//            // do not send message that contains only of spaces
+//            if (completionHandler) {
+//                
+//                completionHandler();
+//            }
+//            
+//            return;
+//        }
+//        
+//        NSString *dialogID = userInfo[kQMPushNotificationDialogIDKey];
+//        
+//        __block UIBackgroundTaskIdentifier task = [application beginBackgroundTaskWithExpirationHandler:^{
+//            
+//            [application endBackgroundTask:task];
+//            task = UIBackgroundTaskInvalid;
+//        }];
+//        
+//        // Do the work associated with the task.
+//        ILog(@"Started background task timeremaining = %f", [application backgroundTimeRemaining]);
+//        
+//        [[[QMCore instance].chatService fetchDialogWithID:dialogID] continueWithBlock:^id _Nullable(BFTask<QBChatDialog *> * _Nonnull t) {
+//            
+//            QBChatDialog *chatDialog = t.result;
+//            if (chatDialog != nil) {
+//                
+//                NSUInteger opponentUserID = [userInfo[kQMPushNotificationUserIDKey] unsignedIntegerValue];
+//                
+//                if (chatDialog.type == QBChatDialogTypePrivate
+//                    && ![[QMCore instance].contactManager isFriendWithUserID:opponentUserID]) {
+//                    
+//                    if (completionHandler) {
+//                        
+//                        completionHandler();
+//                    }
+//                    
+//                    return nil;
+//                }
+//                
+//                return [[[QMCore instance].chatManager sendBackgroundMessageWithText:text toDialogWithID:dialogID] continueWithBlock:^id _Nullable(BFTask * _Nonnull messageTask) {
+//                    
+//                    if (!messageTask.isFaulted
+//                        && application.applicationIconBadgeNumber > 0) {
+//                        
+//                        application.applicationIconBadgeNumber = 0;
+//                    }
+//                    
+//                    [application endBackgroundTask:task];
+//                    task = UIBackgroundTaskInvalid;
+//                    
+//                    return nil;
+//                }];
+//            }
+//            
+//            return nil;
+//        }];
+//    }
+//    
+//    if (completionHandler) {
+//        
+//        completionHandler();
+//    }
+//}
 
 #pragma mark - QMPushNotificationManagerDelegate protocol
 
