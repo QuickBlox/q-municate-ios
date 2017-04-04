a5591285e20fca5f94ef50698aba9abcd0274fdd
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-04-04 14:41:47 +0300

    added category for QMMediaPresenter

---
+++ Q-municate/Classes/Tools/QMMediaController/QMMediaPresenter+QBChatAttachment.h
@@ -0,0 +1,15 @@
+//
+//  QMMediaPresenter+QBChatAttachment.h
+//  Q-municate
+//
+//  Created by Vitaliy Gurkovsky on 3/28/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import "QMChatViewController.h"
+
+@interface QMMediaPresenter (QBChatAttachment)
+
+@property (strong, nonatomic) QBChatAttachment *attachment;
+
+@end
