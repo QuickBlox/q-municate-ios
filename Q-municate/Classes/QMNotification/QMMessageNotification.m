9dead0d2fe670702be465c8cede4bc176b8d5394
pro100Andrey <this.andrey@gmail.com>
2017-06-09 16:29:59 +0300

    updated code style

--- Q-municate/Classes/QMNotification/QMMessageNotification.m
+++ Q-municate/Classes/QMNotification/QMMessageNotification.m
@@ -55,24 +55,24 @@ const CGRect QMMessageNotificationIconRect = (CGRect){(CGPoint){0,0}, (CGSize){3
         
         @weakify(self);
         
-        self.imageOperation = [[QMImageLoader instance] downloadImageWithURL:iconImageURL
-                                             transform:[QMImageTransform transformWithSize:CGSizeMake(30, 30) isCircle:YES]
-                                               options:SDWebImageHighPriority
-                                              progress:nil
-                                             completed:^(UIImage * _Nullable __unused image,
-                                                         UIImage * _Nullable transfomedImage,
-                                                         NSError * _Nullable __unused error,
-                                                         SDImageCacheType __unused cacheType,
-                                                         BOOL __unused finished,
-                                                         NSURL * _Nonnull __unused imageURL) {
-            @strongify(self);
-            
-            if (transfomedImage != nil) {
-                
-                self.messageNotification.iconImage = transfomedImage;
-            }
-            
-        }];
+        self.imageOperation =
+        [[QMImageLoader instance]
+         downloadImageWithURL:iconImageURL
+         transform:[QMImageTransform transformWithSize:CGSizeMake(30, 30) isCircle:YES]
+         options:SDWebImageHighPriority
+         progress:nil
+         completed:^(UIImage * _Nullable __unused image,
+                     UIImage * _Nullable transfomedImage,
+                     NSError * _Nullable __unused error,
+                     SDImageCacheType __unused cacheType,
+                     BOOL __unused finished,
+                     NSURL * _Nonnull __unused imageURL) {
+             @strongify(self);
+             
+             if (transfomedImage != nil) {
+                 self.messageNotification.iconImage = transfomedImage;
+             }
+         }];
     }
     
     if (buttonHandler != nil) {
