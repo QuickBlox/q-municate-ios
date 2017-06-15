a605d1d5da2b92e5afd292dffa420cf08a14ed0e
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-06-15 14:31:35 +0300

    pod update

--- Pods/Headers/Public/SexyTooltip/SexyTooltip.h
+++
@@ -1,1 +0,0 @@
-../../../SexyTooltip/SexyTooltip/SexyTooltip.h
--- Pods/Target Support Files/SexyTooltip/SexyTooltip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SexyTooltip : NSObject
-@end
-@implementation PodsDummy_SexyTooltip
-@end
