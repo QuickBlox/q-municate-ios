c30fee4a0b71f4df889a8c9f704523d34d44caa7
pro100Andrey <this.andrey@gmail.com>
2017-09-26 15:02:15 +0300

    Revert "build version up"
    This reverts commit dfc8c05a088c80bd6aefaa2dfb6d82cc7c74507d.

--- Q-municate/Scenes/Main/Contacts/View Controllers/QMContactsViewController/QMContactsViewController.m
+++ Q-municate/Scenes/Main/Contacts/View Controllers/QMContactsViewController/QMContactsViewController.m
@@ -135,15 +135,17 @@ QMUsersServiceDelegate
     
     
 #ifdef __IPHONE_11_0
-    
-    if (@available(iOS 11.0, *)) {
-        self.navigationItem.searchController = self.searchController;
-        self.navigationItem.hidesSearchBarWhenScrolling = NO;
+    if (iosMajorVersion() >= 11) {
+        
+        if (@available(iOS 11.0, *)) {
+            self.navigationItem.searchController = self.searchController;
+            self.navigationItem.hidesSearchBarWhenScrolling = NO;
+        }
+        
     }
     else {
         self.tableView.tableHeaderView = self.searchController.searchBar;
     }
-    
 #else
     self.tableView.tableHeaderView = self.searchController.searchBar;
 #endif
@@ -213,20 +215,18 @@ QMUsersServiceDelegate
         }];
     };
 }
+#ifdef __IPHONE_11_0
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
     
     [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
-#ifdef __IPHONE_11_0
-        if (@available(iOS 11.0, *)) {
-            self.searchController.active = NO;
-        }
-#endif
+        self.searchController.active = NO;
     } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         
     }];
     
     [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
 }
+#endif
 
 //MARK: - Update items
 
