459fbfdc458b85a0e05e02d9383b864da891f74f
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-10-12 16:00:02 +0300

    added QMShareCollectionViewController

---
+++ QMShareExtension/View Controllers/QMShareCollectionViewController/QMShareCollectionViewController.h
@@ -0,0 +1,14 @@
+//
+//  QMShareCollectionViewController.h
+//  QMShareExtension
+//
+//  Created by Vitaliy Gurkovsky on 10/9/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QMShareCollectionViewController : UICollectionViewController
+
+@end
+ 
