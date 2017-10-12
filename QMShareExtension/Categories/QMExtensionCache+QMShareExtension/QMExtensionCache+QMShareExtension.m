b11523ebd5817a0663f1cf7edf005fbc97e06cb1
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-10-12 15:54:20 +0300

    added category for share extension cache

---
+++ QMShareExtension/Categories/QMExtensionCache+QMShareExtension/QMExtensionCache+QMShareExtension.m
@@ -0,0 +1,26 @@
+//
+//  QMExtensionCache+QMShareExtension.m
+//  QMShareExtension
+//
+//  Created by Vitaliy Gurkovsky on 10/12/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import "QMExtensionCache+QMShareExtension.h"
+
+@implementation QMExtensionCache (QMShareExtension)
+
++ (QBUUser *)userWithID:(NSUInteger)userID {
+    
+    __block QBUUser *user = nil;
+    [[self.usersCache allUsers] enumerateObjectsUsingBlock:^(QBUUser * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.ID == userID) {
+            user = obj;
+            *stop = YES;
+        }
+    }];
+    
+    return user;
+}
+
+@end
