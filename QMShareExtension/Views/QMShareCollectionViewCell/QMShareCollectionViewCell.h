2d18daa903e90d660357294555c7aac49ce896ec
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-10-12 16:17:29 +0300

    added cells

---
+++ QMShareExtension/Views/QMShareCollectionViewCell/QMShareCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  QMShareCollectionViewCell.h
+//  QMShareExtension
+//
+//  Created by Vitaliy Gurkovsky on 10/9/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QMShareViewProtocol.h"
+
+@interface QMShareCollectionViewCell : UICollectionViewCell <QMShareViewProtocol>
+
+@end
