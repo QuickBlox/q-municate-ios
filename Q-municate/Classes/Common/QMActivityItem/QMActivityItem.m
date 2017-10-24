f752002e155c0199a973e4e0faef5f068b485475
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-10-24 16:13:56 +0300

    added QMActivityItem for activity controller

---
+++ Q-municate/Classes/Common/QMActivityItem/QMActivityItem.m
@@ -0,0 +1,108 @@
+//
+//  QMActivityItem.m
+//  Q-municate
+//
+//  Created by Vitaliy Gurkovsky on 10/18/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import "QMActivityItem.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface QMActivityItem()
+
+@property (nonatomic) id placeholderItem;
+@property (nonatomic, copy) NSString *typeIdentifier;
+
+@end
+
+@implementation QMActivityItem
+
+//MARK - NSObject
+- (instancetype)initWithPlaceholderItem:(id)placeholderItem
+                         typeIdentifier:(NSString *)typeIdentifier {
+    
+    self = [super init];
+    
+    if (self) {
+        _placeholderItem = placeholderItem;
+        _typeIdentifier = typeIdentifier ? [typeIdentifier copy] : typeIdentifierForActivityItem(placeholderItem);
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithURL:(NSURL *)URL {
+    return [self initWithPlaceholderItem:URL
+                          typeIdentifier:URL.isFileURL ? (NSString *)kUTTypeFileURL : (NSString *)kUTTypeURL];
+}
+
+- (instancetype)initWithString:(NSString *)string {
+    return [self initWithPlaceholderItem:string
+                          typeIdentifier:(NSString *)kUTTypePlainText];
+}
+
+- (instancetype)initWithImage:(UIImage *)image {
+    
+    return [self initWithPlaceholderItem:image
+                          typeIdentifier:(NSString *)kUTTypeImage];
+}
+
+- (instancetype)initWithData:(NSData *)data
+              typeIdentifier:(NSString *)typeIdentifier {
+    
+    return [self initWithPlaceholderItem:data
+                          typeIdentifier:typeIdentifier];
+}
+//MARK: - UIActivityItemSource
+
+- (id)activityViewControllerPlaceholderItem:(UIActivityViewController *)__unused activityViewController {
+    return self.placeholderItem;
+}
+
+- (nullable id)activityViewController:(UIActivityViewController *)__unused activityViewController
+                  itemForActivityType:(nullable UIActivityType)__unused activityType {
+    
+    NSExtensionItem *item = [[NSExtensionItem alloc] init];
+    
+    NSString *typeIdentifier =
+    [self.placeholderItem isKindOfClass:NSURL.class] ?
+    typeIdentifierForActivityItem(self.placeholderItem) :
+    self.typeIdentifier;
+    
+    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithItem:self.placeholderItem
+                                                         typeIdentifier:typeIdentifier];
+    item.attachments = @[itemProvider];
+    
+    return item;
+}
+
+- (NSString *)activityViewController:(UIActivityViewController *)__unused activityViewController
+   dataTypeIdentifierForActivityType:(NSString *)__unused activityType {
+    return self.typeIdentifier;
+}
+
+static NSString *typeIdentifierForActivityItem(id item) {
+    if ([item isKindOfClass:[NSURL class]]) {
+        NSURL *URL = (NSURL *)item;
+        
+        if (URL.isFileURL) {
+             return (NSString *)kUTTypeFileURL;
+        }
+        
+        return (NSString *)kUTTypeURL;
+    }
+    else if ([item isKindOfClass:[NSString class]]) {
+        return (NSString *)kUTTypePlainText;
+    }
+    else if ([item isKindOfClass:[UIImage class]]) {
+        return (NSString *)kUTTypeImage;
+    }
+    else if ([item isKindOfClass:NSData.class]) {
+        return (NSString *)kUTTypeData;
+    }
+    else {
+        return nil;
+    }
+}
+@end
