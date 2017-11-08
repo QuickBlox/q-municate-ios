c6c7b09a103c871fb59fdbc66923ea01d1aa2e16
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-11-08 13:53:24 +0200

    added main classes for share extension

---
+++ QMShareExtension/View Controllers/QMShareRouterViewController.h
@@ -0,0 +1,14 @@
+//
+//  QMShareRouterViewController.h
+//  Q-municate
+//
+//  Created by Vitaliy Gurkovsky on 11/4/17.
+//  Copyright © 2017 Quickblox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface QMShareRouterViewController : UIViewController
+
+@end
