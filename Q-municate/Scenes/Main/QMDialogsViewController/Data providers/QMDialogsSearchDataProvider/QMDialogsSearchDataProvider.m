b62793806c719d2bbb91613a9a4eb601e4d55bbb
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-11-08 13:48:15 +0200

    updates due to refactored QMDataSource

--- Q-municate/Scenes/Main/QMDialogsViewController/Data providers/QMDialogsSearchDataProvider/QMDialogsSearchDataProvider.m
+++ Q-municate/Scenes/Main/QMDialogsViewController/Data providers/QMDialogsSearchDataProvider/QMDialogsSearchDataProvider.m
@@ -9,6 +9,8 @@
 #import "QMDialogsSearchDataProvider.h"
 #import "QMDialogsSearchDataSource.h"
 #import "QMSearchProtocols.h"
+#import "QMTableViewDataSource.h"
+
 #import "QMCore.h"
 
 static NSString *const kQMDialogsSearchDescriptorKey = @"name";
@@ -22,7 +24,7 @@ static NSString *const kQMDialogsSearchDescriptorKey = @"name";
         return;
     }
     
-    QMSearchDataSource <QMDialogsSearchDataSourceProtocol> *dataSource = (id)self.dataSource;
+    QMTableViewSearchDataSource <QMDialogsSearchDataSourceProtocol> *dataSource = (id)self.dataSource;
     
     if (searchText.length == 0) {
         
@@ -43,7 +45,7 @@ static NSString *const kQMDialogsSearchDescriptorKey = @"name";
         NSPredicate *dialogsSearchPredicate = [NSPredicate predicateWithFormat:@"SELF.name CONTAINS[cd] %@", searchText];
         NSMutableArray *dialogsSearchResult = [NSMutableArray arrayWithArray:[dialogs filteredArrayUsingPredicate:dialogsSearchPredicate]];
         
-        [dataSource setItems:dialogsSearchResult];
+        [dataSource replaceItems:dialogsSearchResult];
         
         dispatch_async(dispatch_get_main_queue(), ^{
             
