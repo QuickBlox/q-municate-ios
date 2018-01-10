53678e6fbbdc72f8074e8d12a606eb9dec9da4bf
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2018-01-10 17:57:14 +0200

    added privacy usage description for contacts(this fixes crash with saving image as contacts's photo)

--- Q-municate/Categories/QBSettings/QBSettings+Qmunicate.h
+++ Q-municate/Categories/QBSettings/QBSettings+Qmunicate.h
@@ -15,7 +15,7 @@ typedef NS_ENUM(NSUInteger, QMApplicationZone) {
     QMApplicationZoneQA,
 };
 
-static const QMApplicationZone QMCurrentApplicationZone = QMApplicationZoneDevelopment1;
+static const QMApplicationZone QMCurrentApplicationZone = QMApplicationZoneDevelopment;
 
 @interface QBSettings (Qmunicate)
 
