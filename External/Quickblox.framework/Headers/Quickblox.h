38d7f0bc4b37072d9bcb524283f6b5a5542dadb7
Andrey <this.andrey@gmail.com>
2018-01-23 16:03:46 +0200

    Updated SDK

--- External/Quickblox.framework/Headers/Quickblox.h
+++ External/Quickblox.framework/Headers/Quickblox.h
@@ -58,5 +58,5 @@
 #import <Quickblox/QBUpdateUserParameters.h>
 #import <Quickblox/QBUUser.h>
 
-/// Framework version 2.14-5
+/// Framework version 2.15
 FOUNDATION_EXPORT NSString * const QuickbloxFrameworkVersion;
